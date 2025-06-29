0dab3ce4d3bc562866495c213bd7468836107b3e
Lau000111 <lau000111.ll@gmail.com>
2025-06-29 22:35:44 +0200

    initial commit

---
+++ src/app/pages/tutorial/tutorial.ts
@@ -0,0 +1,139 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonAvatar,
+  IonLabel,
+  IonSegment,
+  IonSegmentButton,
+  MenuController,
+} from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
+import { addIcons } from 'ionicons';
+import { 
+  arrowForward, 
+  close, 
+  search, 
+  calendar, 
+  people, 
+  business, 
+  person, 
+  chatbubble,
+  home,
+  folder,
+  copy,
+  chatbubbles,
+  personCircle
+} from 'ionicons/icons';
+
+@Component({
+    selector: 'page-tutorial',
+    templateUrl: 'tutorial.html',
+    styleUrls: ['./tutorial.scss'],
+    imports: [
+      FormsModule,
+      IonHeader, 
+      IonToolbar, 
+      IonButtons, 
+      IonButton, 
+      IonContent, 
+      IonIcon,
+      IonCard,
+      IonCardContent,
+      IonGrid,
+      IonRow,
+      IonCol,
+      IonAvatar,
+      IonLabel,
+      IonSegment,
+      IonSegmentButton
+    ]
+})
+export class TutorialPage {
+  selectedTab = 'news';
+
+  constructor(
+    public menu: MenuController,
+    public router: Router,
+    public storage: Storage
+  ) {
+    addIcons({
+      arrowForward,
+      close,
+      search,
+      calendar,
+      people,
+      business,
+      person,
+      chatbubble,
+      home,
+      folder,
+      copy,
+      chatbubbles,
+      personCircle
+    });
+  }
+
+  startApp() {
+    this.router
+      .navigateByUrl('/app/tabs/schedule', { replaceUrl: true })
+      .then(() => this.storage.set('ion_did_tutorial', true));
+  }
+
+  onSegmentChanged(event: any) {
+    this.selectedTab = event.detail.value;
+  }
+
+  navigateToFeature(feature: string) {
+    // Navigate to different features based on the card clicked
+    switch(feature) {
+      case 'practices':
+        this.router.navigate(['/app/tabs/schedule']);
+        break;
+      case 'events':
+        this.router.navigate(['/app/tabs/schedule']);
+        break;
+      case 'clients':
+        this.router.navigate(['/app/tabs/speakers']);
+        break;
+      case 'institutions':
+        this.router.navigate(['/app/tabs/about']);
+        break;
+      case 'profile':
+        this.router.navigate(['/app/tabs/about']);
+        break;
+      case 'chat':
+        this.router.navigate(['/app/tabs/about']);
+        break;
+      default:
+        this.startApp();
+    }
+  }
+
+  ionViewWillEnter() {
+    this.storage.get('ion_did_tutorial').then(res => {
+      if (res === true) {
+        this.router.navigateByUrl('/app/tabs/schedule', { replaceUrl: true });
+      }
+    });
+
+    this.menu.enable(false);
+  }
+
+  ionViewWillLeave() {
+    // enable the root left menu when leaving the tutorial page
+    this.menu.enable(true);
+  }
+}
